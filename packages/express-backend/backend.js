09493f686f3668c9e7c3061f149b859321da62be
James Irwin <jamesirwin0211@gmail.com>
2024-10-09 21:58:15 -0700

    IE2 Step 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,9 @@ return users["users_list"].filter(
 );
 }
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] == id);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -45,27 +48,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
+const name = req.query.name;
+if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+} else {
+    res.send(users);
+}
+})
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result == undefined) {
+        res.status(404).send("Resource not found.");
     } else {
-      res.send(users);
-    }
-  });
-
-
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
         res.send(result);
-    } else {
-        res.send(users);
     }
-  })
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`
